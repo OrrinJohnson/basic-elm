6d07598a7812a3f777e823d559e4d4b7a3eb3652
OrrinJohnson <orrin24johnson@gmail.com>
2020-12-08 06:34:49 -0800

    more function chaining

--- src/Playground.elm
+++ src/Playground.elm
@@ -15,11 +15,14 @@ computeSpeed distance time =
 computeTime startTime endTime =
     endTime - startTime
 
+add a b =
+    a + b
+
+multiply c d =
+    c * d
+
+divide e f =
+    e / f
+
 main = -- this is also a function. 
-    computeTime 2 3
-        |> computeSpeed 7.67 -- |> is a forward function application operator
-        |> escapeEarth 11
-        |> Html.text -- these are known as partial function applications.
-        -- this is a more concise way of calling all of these functions
-        -- as opposed to calling them in a chain like:
-        -- Html.text (escapeEarth 11 (computeSpeed 7.67 (computeTime 2 3)))
+    Html.text (String.fromFloat (add 5 (multiply 10 (divide 30 10))))
