c259a056caa95ec1c55cd8d98b036f0964cd4034
OrrinJohnson <orrin24johnson@gmail.com>
2020-12-08 07:07:11 -0800

    cleaned out the conditional statements with case statement

--- src/Weekdays.elm
+++ src/Weekdays.elm
@@ -1,23 +1,24 @@
 module Weekdays exposing (main)
 import Html
 
-weekday dayInNumber =
-    if dayInNumber == 0 then
-        "Sunday"
-    else if dayInNumber == 1 then
-        "Monday"
-    else if dayInNumber == 2 then
-        "Tuesday"
-    else if dayInNumber == 3 then
-        "Wednesday"
-    else if dayInNumber == 4 then
-        "Thursday"
-    else if dayInNumber == 5 then
-        "Friday"
-    else if dayInNumber == 6 then
-        "Saturday"
-    else
-        "Unknown day"
+weekday correspondingNumber =
+    case correspondingNumber of
+        0 ->
+            "Sunday"
+        1 ->
+            "Monday"
+        2 ->
+            "Tuesday"
+        3 ->
+            "Wednesday"
+        4 ->
+            "Thursday"
+        5 ->
+            "Friday"
+        6 ->
+            "Saturday"
+        _ ->
+            "Unknown day"
 
 main =
-    Html.text <| weekday 5
+    Html.text <| weekday 3
